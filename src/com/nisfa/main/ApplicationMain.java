5ddc20b717e05e8e61f77374c6c5e0dd26272782
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-07-31 13:06:57 +0530

    Welcome Java

---
+++ src/com/nisfa/main/ApplicationMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.nisfa.main;
+
+/**
+ * @author NisfaYasam
+ *
+ */
+public class ApplicationMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Happy Learning");
+
+	}
+
+}
